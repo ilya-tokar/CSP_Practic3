4e6af314b55638721312aae8fa0396aaefd6c7d5
Ilya Tokar <itoakr98@gmail.com>
2020-11-21 17:34:25 +0300

    create calcul nio

--- src/com/company/RunnerTaskThread.java
+++ src/com/company/RunnerTaskThread.java
@@ -1,51 +1,79 @@
 package com.company;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 import java.util.Iterator;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class RunnerTaskThread extends Thread {
 
-    private ConcurrentHashMap<String, Task> tasks = null;
+    private ConcurrentHashMap<SocketChannel, ByteBuffer> tasks = null;
 
-    public RunnerTaskThread(ConcurrentHashMap<String, Task> tasks) {
+    public RunnerTaskThread(ConcurrentHashMap<SocketChannel, ByteBuffer> tasks) {
         this.tasks = tasks;
     }
 
     public void run() {
         while (true){
-            if (tasks.size() != 0){
-                Iterator<ConcurrentHashMap.Entry<String, Task>> itr = tasks.entrySet().iterator();
-                while (itr.hasNext()) {
-                    ConcurrentHashMap.Entry<String, Task> entry = itr.next();
-                    String key = entry.getKey();
-                    Task t = entry.getValue();
-
-                    String[] arr = t.task.split(" ", 3);
-                    int a = Integer.parseInt(arr[0]);
-                    int b = Integer.parseInt(arr[2]);
-                    switch (arr[1]) {
-                        case "+":
-                            t.result = (a + b);
-                            t.flag = true;
-                            break;
-                        case "-":
-                            t.result = (a - b);
-                            t.flag = true;
-                            break;
-                        case "*":
-                            t.result = (a * b);
-                            t.flag = true;
-                            break;
-                        case "/":
-                            t.result = (a / b);
-                            t.flag = true;
-                            break;
-                        default:
-                            System.err.println("Ошибка введенных данных");
+            // Iterate through sockets to see f any socket has anything to say
+            tasks.forEach((socketCh, byteBuffer) -> {
+                try {
+                    int data = socketCh.read(byteBuffer);
+                    if (data == -1) {
+                        closeSocket(socketCh);
+                    } else if (data != 0) { // 0 means socket has nothing to say
+                        byteBuffer.flip();
+                        System.out.println(byteBuffer);
+                        Calculates(byteBuffer);
+                        while (byteBuffer.hasRemaining()) {
+                            socketCh.write(byteBuffer);
+                        }
+                        byteBuffer.compact();
                     }
+                } catch (IOException e) {
+                    closeSocket(socketCh);
+                    throw new UncheckedIOException(e);
                 }
-            }
+            });
         }
     }
+
+    private void closeSocket(SocketChannel socketCh) {
+        try {
+            socketCh.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    //Плохо работает парсер точнее вообще
+    // не работает выводит ошибки
+
+    public void Calculates(ByteBuffer byteBuffer){
+        String v = new String(byteBuffer.array() , Charset.forName("UTF-8") );
+        String[] arr = v.replace("\n","").split(" ", 3);
+        int a = Integer.parseInt(arr[0]);
+        int b = Integer.parseInt(arr[2]);
+        switch (arr[1]) {
+            case "+":
+                byteBuffer.put(ByteBuffer.allocate(a+b));
+                break;
+            case "-":
+                byteBuffer.put(ByteBuffer.allocateDirect(a-b));
+                break;
+            case "*":
+                byteBuffer.put(ByteBuffer.allocateDirect(a*b));
+                break;
+            case "/":
+                byteBuffer.put(ByteBuffer.allocateDirect(a/b));
+                break;
+            default:
+                System.err.println("Ошибка введенных данных");
+        }
+    }
+
 }
--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -1,37 +1,46 @@
 package com.company;
 
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 
 public class Server {
 
-    public static void main(String[] args) {
-        ConcurrentHashMap<String, Task> tasks = new ConcurrentHashMap<String, Task>();
+    public static void main(String[] args) throws IOException {
 
-        if(args.length != 1){
+        if(args.length == 0){
             System.err.println("Usage: java EchoServer <port number>");
-            System.exit(1);
+            System.exit(0);
         }
 
+        final Map<SocketChannel, ByteBuffer> sockets = new ConcurrentHashMap<>();
         int Port = Integer.parseInt(args[0]);
 
-        RunnerTaskThread runnerTaskThread = new RunnerTaskThread(tasks);
-        runnerTaskThread.start();
-        ResultTaskThread resultTaskThread = new ResultTaskThread(tasks);
-        resultTaskThread.start();
+        final ServerSocketChannel serverSocket = ServerSocketChannel.open();
+        // Binding this server on the port
+        serverSocket.bind(new InetSocketAddress(Port));
+        serverSocket.configureBlocking(false); // Make Server nonBlocking
 
-        try(
-                ServerSocket serverSocket = new ServerSocket(Port);
-        ) {
-            while (true) {
-                ServerThread t = new ServerThread(serverSocket.accept(), tasks);
-                t.start();
+        RunnerTaskThread runnerTaskThread = new RunnerTaskThread((ConcurrentHashMap<SocketChannel, ByteBuffer>) sockets);
+        runnerTaskThread.start();
+
+        while (true) {
+
+            final SocketChannel socket = serverSocket.accept();
+
+            if(socket != null){
+               socket.configureBlocking(false);
+               sockets.put(socket, ByteBuffer.allocate(30));
             }
 
-        } catch (IOException e) {
-            e.printStackTrace();
+            sockets.keySet().removeIf((socketChannel) -> !socketChannel.isOpen());
+            // Remove socketChannel which is not opened
+
         }
     }
 }
